29c8b0f89b5db06503e99950de7287db5f105671
OnilBlanco26 <onilblanco26@gmail.com>
2023-03-01 22:50:52 -0300

    Adding field validations and middlewares

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,9 +1,19 @@
 const {Router} = require('express')
+const { check } = require('express-validator')
 const { createUser } = require('../controllers/users.controller')
+const { validateUserByEmail } = require('../middlewares/users.middleware')
+const { validateFields } = require('../middlewares/validateField.middleware')
 
 const router = Router()
 
-router.post('/signup', createUser)
+router.post('/signup', [
+    check('name', 'The username must be mandatory').not().isEmpty(),
+    check('email', 'The email must be mandatory').not().isEmpty(),
+    check('email', 'The email must be a correct format').isEmail(),
+    check('password', 'The passwrod must be mandatory').not().isEmpty(),
+    validateUserByEmail,
+    validateFields,
+], createUser)
 
 // router.post('/login', login)
 
