905ea6c8a400245838208fdf1c17bb21abfffa5e
OnilBlanco26 <onilblanco26@gmail.com>
2023-03-01 22:05:47 -0300

    Defining models

---
+++ models/meals.model.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+const { db } = require('../database/db');
+
+const Meal = db.define('meal', {
+  id: {
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  restaurantId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+  },
+});
+
+module.exports = Meal;
