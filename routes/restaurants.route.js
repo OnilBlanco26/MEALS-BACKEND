ed7a26ebf7a5181206004c41bf92bbeb56b592eb
OnilBlanco26 <onilblanco26@gmail.com>
2023-03-02 11:24:49 -0300

    Using routes for searches

--- routes/restaurants.route.js
+++ routes/restaurants.route.js
@@ -1,9 +1,16 @@
 const { Router } = require("express");
-const { createRestaurant } = require("../controllers/restaurants.controller");
+const { createRestaurant, getAllRestaurants, findRestaurantById } = require("../controllers/restaurants.controller");
+const { getRestaurantById } = require("../middlewares/restaurants.middleware");
 
 const router = new Router()
 
 router.post('/', createRestaurant)
+
+router.get('/', getAllRestaurants)
+
+router.get('/:id', getRestaurantById, findRestaurantById)
+
+router.patch('/:id', updateRestaurant)
 
 
 module.exports = {
