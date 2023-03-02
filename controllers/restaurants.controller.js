f701e1d6335bcb0b29b9dc587fe9d5cce5c9d516
OnilBlanco26 <onilblanco26@gmail.com>
2023-03-02 16:33:09 -0300

    Functions (createReview, updateReview)

--- controllers/restaurants.controller.js
+++ controllers/restaurants.controller.js
@@ -1,4 +1,5 @@
 const Restaurant = require('../models/restaurants.model')
+const Review = require('../models/reviews.model')
 const AppError = require('../utils/appError')
 const catchAsync = require('../utils/catchAsync')
 
@@ -81,10 +82,79 @@ const deleteRestaurant = catchAsync(async(req, res, next) => {
 
 })
 
+const createReview = catchAsync(async(req,res,next) => {
+    console.log('estoy entrando')
+    const {sessionUser} = req;
+    const {comment, rating} = req.body
+    const {id} = req.params
+
+    if(rating > 5 || rating < 0) {
+        return next(new AppError('The rating must be between 0 and 5', 400))
+    }
+
+    const newReview = await Review.create({
+        userId: sessionUser.id,
+        comment,
+        restaurantId: id,
+        rating
+    })
+
+    console.log(newReview)
+
+    res.status(200).json({
+        status: 'success',
+        message: 'The Review was created succesfully',
+        newReview
+    })
+})
+
+const updateReview = catchAsync(async(req,res,next) => {
+    const {sessionUser} = req;
+    const {restaurantId, id} = req.params 
+    const {comment, rating} = req.body 
+
+    const restaurant = await Restaurant.findOne({
+        where: {
+            id: restaurantId,
+            status: true
+        }
+    })
+
+    if(!restaurant) {
+        return next(new AppError('Restaurant not found', 400))
+    }
+
+    const review = await Review.findOne({
+        where: {
+            id,
+            userId: sessionUser.id,
+            restaurantId,
+            status: 'active'
+        }
+    })
+
+    if(!review) {
+        return next(new AppError('Review not found', 400))
+    }
+
+    const updateR = await review.update({
+        comment,
+        rating
+    })
+
+    res.status(200).json({
+        status: 'success',
+        message: 'The review has been updated correctly',
+        updateR
+    })
+})
+
 module.exports = {
     createRestaurant,
     getAllRestaurants,
     findRestaurantById,
     updateRestaurant,
-    deleteRestaurant
+    deleteRestaurant,
+    createReview,
+    updateReview
 }
--- routes/restaurants.route.js
+++ routes/restaurants.route.js
@@ -1,35 +1,68 @@
-const { Router } = require("express");
-const { check } = require("express-validator");
-const { createRestaurant, getAllRestaurants, findRestaurantById, updateRestaurant, deleteRestaurant } = require("../controllers/restaurants.controller");
-const { getRestaurantById } = require("../middlewares/restaurants.middleware");
-const { validateFields } = require("../middlewares/validateField.middleware");
+const { Router } = require('express');
+const { check } = require('express-validator');
+const {
+  createRestaurant,
+  getAllRestaurants,
+  findRestaurantById,
+  updateRestaurant,
+  deleteRestaurant,
+  createReview,
+  updateReview,
+} = require('../controllers/restaurants.controller');
+const { getRestaurantById } = require('../middlewares/restaurants.middleware');
+const { protect } = require('../middlewares/users.middleware');
+const { validateFields } = require('../middlewares/validateField.middleware');
 
-const router = new Router()
+const router = new Router();
 
-router.post('/',
-[
+router.post('/reviews/:id', [
+    check('comment', 'The comment must be mandatory').not().isEmpty(),
+    check('rating', 'The rating must be mandatory').not().isEmpty(),
+    check('rating', 'The rating must be mandatory').isNumeric(),
+    protect,
+    getRestaurantById,
+    validateFields
+  ], createReview)
+
+
+router.post(
+  '/',
+  [
     check('name', 'The name must be mandatory').not().isEmpty(),
     check('address', 'The address must be mandatory').not().isEmpty(),
-    validateFields
-] 
-, createRestaurant)
+    validateFields,
+    protect,
+  ],
+  createRestaurant
+);
 
-router.get('/', getAllRestaurants)
+router.get('/', getAllRestaurants);
 
-router.get('/:id', getRestaurantById, findRestaurantById)
+router.get('/:id', getRestaurantById, findRestaurantById);
 
-router.patch('/:id', 
-[
+router.use(protect);
+
+router.patch(
+  '/:id',
+  [
     check('name', 'The name must be mandatory').not().isEmpty(),
     check('address', 'The address must be mandatory').not().isEmpty(),
     getRestaurantById,
-    validateFields
-]
- ,updateRestaurant)
+    validateFields,
+  ],
+  updateRestaurant
+);
 
- router.delete('/:id', getRestaurantById ,deleteRestaurant)
+router.delete('/:id', getRestaurantById, deleteRestaurant);
 
+router.patch('/reviews/:restaurantId/:id', [
+    check('comment', 'The comment must be mandatory').not().isEmpty(),
+    check('rating', 'The rating must be mandatory').not().isEmpty(),
+    check('rating', 'The rating must a numeric').isNumeric(),
+    getRestaurantById,
+    validateFields,
+] ,updateReview)
 
 module.exports = {
-    restaurantsRouter: router
-}
+  restaurantsRouter: router,
+};
