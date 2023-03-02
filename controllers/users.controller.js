0fecc0ece324bcc1b138c19f7dc2d6149e87cb51
OnilBlanco26 <onilblanco26@gmail.com>
2023-03-01 23:08:33 -0300

    Add function login

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -4,35 +4,58 @@ const bcrypt = require('bcryptjs');
 const generateJWT = require('../utils/jwt');
 
 const createUser = catchAsync(async (req, res, next) => {
-    const {name, email, password, role} = req.body
+  const { name, email, password, role } = req.body;
 
-    const user = new User({
-        name: name.toLowerCase(),
-        email: email.toLowerCase(),
-        password,
-        role
-    })
+  const user = new User({
+    name: name.toLowerCase(),
+    email: email.toLowerCase(),
+    password,
+    role,
+  });
 
-    const salt = await bcrypt.genSalt(10)
-    user.password = await bcrypt.hash(password, salt)
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(password, salt);
 
-    await user.save()
+  await user.save();
 
-    const token = await generateJWT(user.id)
+  const token = await generateJWT(user.id);
 
-    res.status(201).json({
-        status: 'success',
-        message: 'The user was created succesfully',
-        token,
-        user: {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            role: user.role
-        }
-    })
+  res.status(201).json({
+    status: 'success',
+    message: 'The user was created succesfully',
+    token,
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    },
+  });
+});
+
+const login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  const { user } = req;
+
+  if (!(await bcrypt.compare(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  const token = await generateJWT(user.id)
+
+  res.status(200).json({
+    status: 'success',
+    token,
+    user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+    }
+  })
 });
 
 module.exports = {
-    createUser
+  createUser,
+  login
 };
