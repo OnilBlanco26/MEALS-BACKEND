8681d6316b8efa1aecbcb504a7739129f27bfd9b
OnilBlanco26 <onilblanco26@gmail.com>
2023-03-02 15:36:57 -0300

    Functions (getAllOrders, getOrderById)

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -2,6 +2,10 @@ const User = require('../models/users.model');
 const catchAsync = require('../utils/catchAsync');
 const bcrypt = require('bcryptjs');
 const generateJWT = require('../utils/jwt');
+const Order = require('../models/orders.model');
+const Restaurant = require('../models/restaurants.model')
+const AppError = require('../utils/appError');
+const Meal = require('../models/meals.model');
 
 const createUser = catchAsync(async (req, res, next) => {
   const { name, email, password, role } = req.body;
@@ -81,9 +85,100 @@ const deleteUser = catchAsync(async(req, res , next) => {
       });
 })
 
+const getOrders= catchAsync(async(req,res,next) => {
+  const {sessionUser} = req;
+
+  const orders = await Order.findAll({
+    attributes: {
+      exclude: ['createdAt', 'updatedAt', 'status']
+    },
+    where: {
+      userId: sessionUser.id,
+      [Sequelize.Op.or]: [{ status: 'active' }, { status: 'completed' }],
+    },include: [
+      {
+        model: Meal,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'status', 'restaurantId']
+        },
+        where : {
+          status: true
+        },
+        include: [
+          {
+            model: Restaurant,
+            attributes: {
+              exclude: ['createdAt', 'updatedAt', 'status']
+            },
+            where: {
+              status: true
+            }
+          }
+        ]
+      }
+    ]
+  })
+
+  if(orders.length === 0) {
+    return next(new AppError('There are no orders in the list', 400))
+  }
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Here is the list of orders',
+    orders
+  })
+})
+
+const getOrderById = catchAsync(async(req,res,next) => {
+  const {sessionUser} = req;
+  const {id} = req.params
+ 
+  const order = await Order.findOne({
+    where: {
+      id,
+      userId: sessionUser.id,
+      [Sequelize.Op.or]: [{ status: 'active' }, { status: 'completed' }],
+    } ,include: [
+      {
+        model: Meal,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'status', 'restaurantId']
+        },
+        where : {
+          status: true
+        },
+        include: [
+          {
+            model: Restaurant,
+            attributes: {
+              exclude: ['createdAt', 'updatedAt', 'status']
+            },
+            where: {
+              status: true
+            }
+          }
+        ]
+      }
+    ]
+  })
+
+  if(!order) {
+    return next(new AppError('The order has not been found', 400))
+  }
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Order found correctly',
+    order
+  })
+})
+
 module.exports = {
   createUser,
   login,
   updateUser,
-  deleteUser
+  deleteUser,
+  getOrders,
+  getOrderById
 };
